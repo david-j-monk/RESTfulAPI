b28008a74bb4f468e697fef2ddbc244ab604ce13
Dave Monk <dm584@canterbury.ac.uk>
2021-02-08 11:41:21 +0000

    Init Commit

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace RESTfulAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>() ;
                 });
     }
 }
